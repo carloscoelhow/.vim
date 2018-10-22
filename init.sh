b8097cda38dff079334293ffbb07e5a08474372d
ivanCanaveral <canaveral.ivan@gmail.com>
2018-10-22 21:02:44 +0200

    included nerd fonts in init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.vim/vimrc ~/.vimrc
+
+git clone https://github.com/ryanoasis/nerd-fonts ~/.fonts/
+bash ~/.fonts/install.sh
